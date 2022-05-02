462f057a38390324f7627d961fd70d0fc27c949f
Timur <altynbaevtimuru@yandex.ru>
2022-05-02 19:34:09 +0300

    lesson 9. create api versions

--- todoList/users/serializers.py
+++ todoList/users/serializers.py
@@ -8,3 +8,10 @@ class UserModelSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'email')
+
+class UserModelFullSerializer(ModelSerializer):
+
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'is_superuser', 'is_staff')
